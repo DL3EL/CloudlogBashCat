6258eb01362b486812e7dbd615cc35115ed1f8b9
DL3EL <dl3el@darc.de>
2023-02-02 14:28:30 +0100

    add fldigi & sparksdr support
    Signed-off-by: DL3EL <dl3el@darc.de>

--- cloudlogbashcat.sh
+++ cloudlogbashcat.sh
@@ -27,6 +27,7 @@ rigMode="MATCH"
 rigOldMode="NO MATCH"
 
 delay=1
+n=0
 
 # load in config ...
 source cloudlogbashcat.conf
@@ -47,7 +48,10 @@ generateRequestXml() {
     echo '<?xml version="1.0"?>'
     echo "<methodCall>"
     echo "    <methodName>$method</methodName>"
-    echo "    <params>"
+    if [[ $rigControlSoftware = "flrig" ]]; then
+# no need of <params> when using fldigi
+	echo "    <params>"
+    fi
 
     indent="    "
     for arg; do
@@ -58,7 +62,9 @@ generateRequestXml() {
         esac
     done
 
-    echo "    </params>"
+    if [[ $rigControlSoftware = "flrig" ]]; then
+	echo "    </params>"
+    fi
     echo "</methodCall>"
 }
 
@@ -98,17 +104,203 @@ while true; do
 			rigMode=$(curl -k $verbose --data "$rpcxml" "$host:$port" 2>/dev/null | xmllint --format --xpath '//value/text()' -)
 			;;
 
+		fldigi)
+			# Get flrig frequency ...
+			rpcxml=$(generateRequestXml "main.get_frequency")
+			rigFreq=$(curl -k $verbose --data "$rpcxml" "$host:$port" 2>/dev/null | xmllint --format --xpath '//value/double/text()' - 2>&1)
+			# freq is delivered as double: 14189860.000000, change format
+			rigFreq=${rigFreq%.*}
+
+			if [[ $? -ne 0 ]]; then
+			    echo "Unable to contact server" >&2
+			    exit 1
+			fi
+
+			# Get flrig mode ...
+			rpcxml=$(generateRequestXml "rig.get_mode")
+			rigMode=$(curl -k $verbose --data "$rpcxml" "$host:$port" 2>/dev/null | xmllint --format --xpath '//value/text()' -)
+			case $rigMode in
+			# CWR, RTTYR, etc is not supported by Cloudlog
+			    CWR)
+				rigMode="CW"
+				;;
+			    USB)
+				rigMode="SSB"
+				;;
+			    LSB)
+				rigMode="SSB"
+				;;
+			    PKTUSB)
+				rigMode="PSK"
+				;;
+			    PKTLSB)
+				rigMode="PSK"
+				;;
+			    RTTYR)
+			    rigMode="RTTY"
+			    ;;
+			esac
+			;;
+		sparksdr)
+			rigMode=$(rigctl -r $host:$port -m2 m)
+			rigFreq=$(rigctl -r $host:$port -m2 f)
+			
+			IFS='-' read -ra  IP <<< "$rigMode"
+			for rigMode in "${IP[@]}";
+# sonst läuft es nicht
+			do
+			    echo "" >/dev/null
+			done
+			# sparksdr delivers not all modes, try to guess them
+			if [ "$rigMode"="" ]; then 
+			    case $rigFreq in
+				136000)
+				    rigMode="USB"
+				    ;;
+				474200)
+				    rigMode="USB"
+				    ;;
+				1836000)
+				    rigMode="FT8"
+				    ;;
+				3573000)
+				    rigMode="FT8"
+				    ;;
+				3576000)
+				    rigMode="FT4"
+				    ;;
+				3568600)
+				    rigMode="WSPR"
+				    ;;
+				5357000)
+				    rigMode="FT8"
+				    ;;
+				5366000)
+				    rigMode="WSPR"
+				    ;;
+				7074000)
+				    rigMode="FT8"
+				    ;;
+				7038600)
+				    rigMode="WSPR"
+				    ;;
+				7047500)
+				    rigMode="FT4"
+				    ;;
+				10136000)
+				    rigMode="FT8"
+				    ;;
+				10138700)
+				    rigMode="WSPR"
+				    ;;
+				10140000)
+				    rigMode="FT4"
+				    ;;
+				14074000)
+				    rigMode="FT8"
+				    ;;
+				14080000)
+				    rigMode="FT4"
+				    ;;
+				14095600)
+				    rigMode="WSPR"
+				    ;;
+				18100000)
+				    rigMode="FT8"
+				    ;;
+				18104000)
+				    rigMode="FT4"
+				    ;;
+				18104600)
+				    rigMode="WSPR"
+				    ;;
+				21074000)
+				    rigMode="FT8"
+				    ;;
+				21094600)
+				    rigMode="WSPR"
+				    ;;
+				21140000)
+				    rigMode="FT4"
+				    ;;
+				24915000)
+				    rigMode="FT8"
+				    ;;
+				24919000)
+				    rigMode="FT4"
+				    ;;
+				24924600)
+				    rigMode="WSPR"
+				    ;;
+				28070000)
+				    rigMode="FT4"
+				    ;;
+				28074000)
+				    rigMode="FT8"
+				    ;;
+				28124600)
+				    rigMode="WSPR"
+				    ;;
+			    esac
+			fi    
+			
+			rigMode=$(echo $rigMode|tr -d '\n')
+			case $rigMode in
+			    CWR)
+				rigMode="CW"
+				;;
+			    USB)
+				rigMode="SSB"
+				;;
+			    LSB)
+				rigMode="SSB"
+				;;
+			    280)
+				rigMode="WSPR"
+				;;
+			    4900)
+				rigMode="JT9"
+				;;
+			    2850)
+				rigMode="SSTV"
+				;;
+			    3000)
+				rigMode="PSK"
+				;;
+			    16000)
+				rigMode="FM"
+				;;
+			    44000)
+				rigMode="USB"
+				;;
+			    PKTUSB)
+				rigMode="PSK"
+				;;
+			    PKTLSB)
+				rigMode="PSK"
+				;;
+    			esac
+			;;
+
 		*)
 			echo "Unknown rig control server type" >&2
 			exit 1
 			;;
 	esac
 
+  if [ "$n" -gt 600 ]; then
+# make sure that at least every 10 min something is pushed to cloudlog, to prevent an error message (rig not responding)
+    rigOldFreq=0
+    n=0
+    [[ $DEBUG -eq 1 ]] && printf  "%s   %s $(date +"%Y/%m/%d %H:%M")\n" $rigFreq $rigMode
+ else 
+    n=$((n+1))
+  fi    
 
 		
-  if [ $rigFreq -ne $rigOldFreq  ] || [ "$rigMode" != "$rigOldMode"  ]; then
+  if [ "$rigFreq" -ne "$rigOldFreq"  ] || [ "$rigMode" != "$rigOldMode"  ]; then
     # rig freq or mode changed, update Cloudlog
-    [[ $DEBUG -eq 1 ]] && printf  "%-10d   %-6s\n" $rigFreq $rigMode
+    [[ $DEBUG -eq 1 ]] && printf  "%s   %s $(date +"%Y/%m/%d %H:%M")\n" $rigFreq $rigMode
     rigOldFreq=$rigFreq
     rigOldMode=$rigMode
 
@@ -124,8 +316,12 @@ while true; do
            \"mode\":\"$rigMode\",
            \"timestamp\":\"$(date -u +"%Y/%m/%d %H:%M")\"
          }" $cloudlogApiUrl >/dev/null 2>&1
+
+    n=0
   fi
 
 	sleep $delay
 done
 
+# Testcall, use in CLI, if script does not show any update in cloudlog
+# curl --insecure --header "Content-Type: application/json" --request POST --data "{ \"key\":\"cl6key2520519d8\",\"radio\":\"HL2 R1\",\"frequency\":\"24911000\",\"mode\":\"FM\",\"timestamp\":\"2023/02/02 12:42\"}" https://192.168.1.1/index.php/api/radio
